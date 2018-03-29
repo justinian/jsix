7ce2898b6ec2222f16991292a0e968fe0e60d2e7
Justin C. Miller <justin@devjustinian.com>
2018-03-29 09:49:38 -0700

    Test for incoming data pointer in kernel_main

--- src/modules/main/debug.s
+++ src/modules/main/debug.s
@@ -3,9 +3,8 @@ section .text
 global do_the_set_registers
 do_the_set_registers:
 	mov rax, 0xdeadbeef0badc0de
-	mov rbx, 0xdeadbeef0badc0de
-	mov rcx, 0xdeadbeef0badc0de
-	mov rdx, 0xdeadbeef0badc0de
+	mov r8, rcx
+	mov r9, rdi
 
 global _halt
 _halt:
--- src/modules/main/main.c
+++ src/modules/main/main.c
@@ -1,15 +1,7 @@
-#include "vga.h"
-
 void do_the_set_registers();
 
 void
-kernel_main()
+kernel_main(void *header)
 {
-	volatile register int foo = 0x1a1b1c10;
-	volatile register int bar = 0;
-
-	terminal_initialize(5);
-	terminal_writestring("YES HELLO THIS IS KERNEL");
-
-	do_the_set_registers();
+	do_the_set_registers(header);
 }
