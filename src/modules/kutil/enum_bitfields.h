799ad8b264bbc14aa7968bb60beba63a74d799df
Justin C. Miller <justin@devjustinian.com>
2018-04-21 02:47:37 -0700

    Add operator| to enum_bitfields

--- src/modules/kutil/enum_bitfields.h
+++ src/modules/kutil/enum_bitfields.h
@@ -19,6 +19,15 @@ operator & (E lhs, E rhs)
 
 template <typename E>
 typename std::enable_if<is_enum_bitfield<E>::value,E>::type
+operator | (E lhs, E rhs)
+{
+	return static_cast<E> (
+		static_cast<typename std::underlying_type<E>::type>(lhs) |
+		static_cast<typename std::underlying_type<E>::type>(rhs));
+}
+
+template <typename E>
+typename std::enable_if<is_enum_bitfield<E>::value,E>::type
 operator ^ (E lhs, E rhs)
 {
 	return static_cast<E> (
@@ -71,4 +80,11 @@ typename std::enable_if<is_enum_bitfield<E>::value,bool>::type
 operator ! (E rhs)
 {
 	return static_cast<typename std::underlying_type<E>::type>(rhs) == 0;
+}
+
+template <typename E>
+typename std::enable_if<is_enum_bitfield<E>::value,bool>::type
+bitfield_contains(E set, E flag)
+{
+	return (set & flag) == flag;
 }
