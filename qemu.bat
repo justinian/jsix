818b51d57cfe696f7035695cd742acdfe249786a
Justin C. Miller <justin@devjustinian.com>
2018-04-21 02:45:33 -0700

    Make qemu.bat log to popcorn.log

--- qemu.bat
+++ qemu.bat
@@ -1,2 +1,3 @@
 call make.bat
-qemu-system-x86_64.exe -bios .\assets\ovmf\x64\OVMF.fd -hda .\build\fs.img -m 512 -vga cirrus
+del popcorn.log
+qemu-system-x86_64.exe -bios .\assets\ovmf\x64\OVMF.fd -hda .\build\fs.img -m 512 -vga cirrus -d guest_errors,int -D popcorn.log -no-reboot
