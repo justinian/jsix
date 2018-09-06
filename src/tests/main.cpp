bc26d7d01d4db88f9751c44795afdbfef45b79fa
Justin C. Miller <justin@devjustinian.com>
2018-09-05 20:17:29 -0700

    Fixing test compilation

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -1,2 +1,9 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+// kutil malloc/free stubs
+#include <malloc.h>
+namespace kutil {
+	void * malloc(size_t n) { return ::malloc(n); }
+	void free(void *p) { ::free(p); }
+}
