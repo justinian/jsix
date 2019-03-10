cd2ccb4e0611ebd3773c20e4fca53c4f7bb3d328
Justin C. Miller <justin@devjustinian.com>
2019-03-10 12:57:43 -0700

    Move QEMU monitor to telnet

--- qemu.sh
+++ qemu.sh
@@ -23,10 +23,18 @@ if [[ -c /dev/kvm ]]; then
 	kvm="-enable-kvm"
 fi
 
-ninja -C "${build}" && \
+if ! ninja -C "${build}"; then
+	exit 1
+fi
+
+if [[ -n $TMUX ]]; then
+	tmux split-window "sleep 1; telnet localhost 45454" &
+fi
+
 exec qemu-system-x86_64 \
 	-drive "if=pflash,format=raw,file=${build}/flash.img" \
 	-drive "format=raw,file=${build}/popcorn.img" \
+	-monitor telnet:localhost:45454,server,nowait \
 	-smp 1 \
 	-m 512 \
 	-d mmu,int,guest_errors \
