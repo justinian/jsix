645938a1940728aa385606567bae2e877813e583
Justin C. Miller <justin@devjustinian.com>
2019-03-17 10:02:57 -0700

    Add bip buffer and constexpr hash

---
+++ src/libraries/kutil/bip_buffer.cpp
@@ -0,0 +1,95 @@
+#include "kutil/assert.h"
+#include "kutil/bip_buffer.h"
+
+namespace kutil {
+
+bip_buffer::bip_buffer() :
+		m_start_a(0),
+		m_start_b(0),
+		m_size_a(0),
+		m_size_b(0),
+		m_buffer_size(0),
+		m_buffer(nullptr)
+	{}
+
+bip_buffer::bip_buffer(uint8_t *buffer, size_t size) :
+		m_start_a(0),
+		m_start_b(0),
+		m_size_a(0),
+		m_size_b(0),
+		m_buffer_size(size),
+		m_buffer(buffer)
+	{}
+
+size_t bip_buffer::reserve(size_t size, void **area)
+{
+	if (m_size_r) {
+		*area = nullptr;
+		return 0;
+	}
+
+	size_t remaining = 0;
+	if (m_size_b) {
+		// If B exists, we're appending there. Get space between
+		// the end of B and start of A.
+		remaining = m_start_a - m_start_b - m_size_b;
+		m_start_r = m_start_b + m_size_b;
+	} else {
+		// B doesn't exist, check the space both before and after A.
+		// If the end of A has enough room for this write, put it there.
+		remaining = m_buffer_size - m_start_a - m_size_a;
+		m_start_r = m_start_a + m_size_a;
+
+		// Otherwise use the bigger of the areas in front of and after A
+		if (remaining < size && m_start_a > remaining) {
+			remaining = m_start_a;
+			m_start_r = 0;
+		}
+	}
+
+	if (!remaining) {
+		*area = nullptr;
+		return 0;
+	}
+
+	m_size_r = (remaining < size) ? remaining : size;
+	*area = &m_buffer[m_start_r];
+	return m_size_r;
+}
+
+void bip_buffer::commit(size_t size)
+{
+	kassert(size <= m_size_r, "Tried to commit more than reserved");
+
+	if (m_start_r == m_start_a + m_size_a) {
+		// We were adding to A
+		m_size_a += size;
+	} else {
+		// We were adding to B
+		kassert(m_start_r == m_start_b + m_size_b, "Bad m_start_r!");
+		m_size_b += size;
+	}
+
+	m_start_r = m_size_r = 0;
+}
+
+size_t bip_buffer::get_block(void **area) const
+{
+	*area = m_size_a ? &m_buffer[m_start_a] : nullptr;
+	return m_size_a;
+}
+
+void bip_buffer::consume(size_t size)
+{
+	kassert(size <= m_size_a, "Consumed more bytes than exist in A");
+	if (size >= m_size_a) {
+		m_size_a = m_size_b;
+		m_start_a = m_start_b;
+		m_size_b = m_start_b = 0;
+	} else {
+		m_size_a -= size;
+		m_start_a += size;
+	}
+}
+
+} // namespace kutil
---
+++ src/libraries/kutil/include/kutil/bip_buffer.h
@@ -0,0 +1,59 @@
+#pragma once
+/// \file bip_buffer.h
+/// A Bip Buffer (bipartite circular buffer). For more on the Bip Buffer structure, see
+/// https://www.codeproject.com/Articles/3479/The-Bip-Buffer-The-Circular-Buffer-with-a-Twist
+
+#include <stddef.h>
+#include <stdint.h>
+
+namespace kutil {
+
+class bip_buffer
+{
+public:
+	/// Default constructor. Creates a zero-size buffer.
+	bip_buffer();
+
+	/// Constructor.
+	bip_buffer(uint8_t *buffer, size_t size);
+
+	/// Reserve an area of buffer for a write.
+	/// \arg size  Requested size, in bytes
+	/// \arg area  [out] Pointer to returned area
+	/// \returns   Size of returned area, in bytes, or 0 on failure
+	size_t reserve(size_t size, void **area);
+
+	/// Commit a pending write started by reserve()
+	/// \arg size  Amount of data used, in bytes
+	void commit(size_t size);
+
+	/// Get a pointer to a block of data in the buffer.
+	/// \arg area  [out] Pointer to the retuned area
+	/// \returns   Size of the returned area, in bytes
+	size_t get_block(void **area) const;
+
+	/// Mark a number of bytes as consumed, freeing buffer space
+	/// \arg size  Number of bytes to consume
+	void consume(size_t size);
+
+	/// Get total amount of data in the buffer.
+	/// \returns  Number of bytes committed to the buffer
+	inline size_t size() const { return m_size_a + m_size_b; }
+
+	/// Get total amount of free buffer remaining
+	/// \returns  Number of bytes of buffer that are free
+	inline size_t free_space() const { return m_buffer_size - size(); }
+
+private:
+	size_t m_start_a;
+	size_t m_start_b;
+	size_t m_start_r;
+	size_t m_size_a;
+	size_t m_size_b;
+	size_t m_size_r;
+
+	const size_t m_buffer_size;
+	uint8_t * const m_buffer;
+};
+
+} // namespace kutil
