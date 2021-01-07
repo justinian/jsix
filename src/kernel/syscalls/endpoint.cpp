d1c0723b44ec756da34592638b83b54354ffd364
Justin C. Miller <justin@devjustinian.com>
2021-01-06 23:16:16 -0800

    [kernel] Fix memory clobbering from endpoint
    The endpoint receive syscalls can block and then write to userspace
    memory. Since the current address space may be different after blocking,
    make sure to only actually write to the user memory after returning to
    the syscall handler - pass values that are on the syscall handler stack
    deeper into the kernel.

--- src/kernel/syscalls/endpoint.cpp
+++ src/kernel/syscalls/endpoint.cpp
@@ -35,7 +35,12 @@ endpoint_receive(j6_handle_t handle, j6_tag_t *tag, size_t *len, void *data)
 	endpoint *e = get_handle<endpoint>(handle);
 	if (!e) return j6_err_invalid_arg;
 
-	return e->receive(tag, len, data);
+	j6_tag_t out_tag = j6_tag_invalid;
+	size_t out_len = 0;
+	j6_status_t s = e->receive(&out_tag, &out_len, data);
+	*tag = out_tag;
+	*len = out_len;
+	return s;
 }
 
 j6_status_t
@@ -51,7 +56,12 @@ endpoint_sendrecv(j6_handle_t handle, j6_tag_t *tag, size_t *len, void *data)
 	if (status != j6_status_ok)
 		return status;
 
-	return e->receive(tag, len, data);
+	j6_tag_t out_tag = j6_tag_invalid;
+	size_t out_len = 0;
+	j6_status_t s = e->receive(&out_tag, &out_len, data);
+	*tag = out_tag;
+	*len = out_len;
+	return s;
 }
 
 } // namespace syscalls
