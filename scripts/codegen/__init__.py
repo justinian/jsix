372325fab7b6f252f0b767b2dc44ac3e224b7b50
Justin C. Miller <justin@devjustinian.com>
2024-08-12 19:26:56 -0700

    [scripts] Fix for missing path seperator in errno.h
    Make sure to add a path seperator after the root in
    codegen.arch_includes.

--- scripts/codegen/__init__.py
+++ scripts/codegen/__init__.py
@@ -5,10 +5,15 @@ supported_architectures = {
 }
 
 def arch_includes(header, root=""):
+    from pathlib import Path
+    root = Path(root)
+    header = Path(header)
+
     prefix = "if"
     for arch, define in supported_architectures.items():
+        path = root / "arch" / arch / header
         cog.outl(f"#{prefix} defined({define})")
-        cog.outl(f"#include <{root}arch/{arch}/{header}>")
+        cog.outl(f"#include <{path}>")
         prefix = "elif"
     cog.outl("#else")
     cog.outl('#error "Unsupported platform"')
